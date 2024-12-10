0c53890081a4235cd6ad6f59dcd100eeef2510db
artchaidez <artchaidez1995@gmail.com>
2024-12-10 13:14:24 -0600

    Screen width fixes; css; fWAR
    Fixed tables in Awards page to fit smaller screens; fixed table headers to be readable in light mode; changed WAR to fWAR

--- leaders.js
+++ leaders.js
@@ -1,22 +1,22 @@
 let battingArray = [
-    {'num': '1', 'name': 'Aaron Judge', 'team': 'NYY', 'AVG': '.322', 'OBP': '.458', 'SLG': '.701', 'wRC+':'218', 'BsR':'-0.4', 'Def':'-9.9','WAR':'11.2'},
-    {'num': '2', 'name': 'Bobby Witt Jr', 'team': 'KCR', 'AVG': '.332', 'OBP': '.389', 'SLG': '.588', 'wRC+':'168', 'BsR':'-4.5', 'Def':'-17.5','WAR':'10.2'},
-    {'num': '3', 'name': 'Shohei Ohtani', 'team': 'LAD', 'AVG': '.310', 'OBP': '.390', 'SLG': '.646', 'wRC+':'181', 'BsR':'9.8', 'Def':'-17.2','WAR':'9.1'},
-    {'num': '4', 'name': 'Juan Soto', 'team': 'NYY', 'AVG': '.288', 'OBP': '.419', 'SLG': '.569', 'wRC+':'180', 'BsR':'-3.8', 'Def':'-6.1','WAR':'8.1'},
-    {'num': '5', 'name': 'Gunner Henderson', 'team': 'BAL', 'AVG': '.281', 'OBP': '.364', 'SLG': '.529', 'wRC+':'155', 'BsR':'4.0', 'Def':'4.7','WAR':'8.0'}
+    {'num': '1', 'name': 'Aaron Judge', 'team': 'NYY', 'AVG': '.322', 'OBP': '.458', 'SLG': '.701', 'wRC+':'218', 'BsR':'-0.4', 'Def':'-9.9','fWAR':'11.2'},
+    {'num': '2', 'name': 'Bobby Witt Jr', 'team': 'KCR', 'AVG': '.332', 'OBP': '.389', 'SLG': '.588', 'wRC+':'168', 'BsR':'-4.5', 'Def':'-17.5','fWAR':'10.2'},
+    {'num': '3', 'name': 'Shohei Ohtani', 'team': 'LAD', 'AVG': '.310', 'OBP': '.390', 'SLG': '.646', 'wRC+':'181', 'BsR':'9.8', 'Def':'-17.2','fWAR':'9.1'},
+    {'num': '4', 'name': 'Juan Soto', 'team': 'NYY', 'AVG': '.288', 'OBP': '.419', 'SLG': '.569', 'wRC+':'180', 'BsR':'-3.8', 'Def':'-6.1','fWAR':'8.1'},
+    {'num': '5', 'name': 'Gunner Henderson', 'team': 'BAL', 'AVG': '.281', 'OBP': '.364', 'SLG': '.529', 'wRC+':'155', 'BsR':'4.0', 'Def':'4.7','fWAR':'8.0'}
 ]
 
-let batterHeader = ['#', 'Name', 'Team', 'AVG', 'OBP', 'SLG', 'wRC+', 'BsR', 'Def', 'WAR']
+let batterHeader = ['#', 'Name', 'Team', 'AVG', 'OBP', 'SLG', 'wRC+', 'BsR', 'Def', 'fWAR']
 
 let pitchingArray = [
-    {'num': '1', 'name': 'Chris Sale', 'team': 'ATL', 'IP': '177.2', 'K%': '32.1', 'BB%': '5.6', 'GB%':'44.8', 'ERA':'2.38', 'FIP':'2.09','WAR':'6.4'},
-    {'num': '2', 'name': 'Tarik Skubal', 'team': 'DET', 'IP': '192.0', 'K%': '30.3', 'BB%': '4.6', 'GB%':'45.7', 'ERA':'2.39', 'FIP':'2.49','WAR':'5.9'},
-    {'num': '3', 'name': 'Zack Wheeler', 'team': 'PHI', 'IP': '200.0', 'K%': '29.5', 'BB%': '6.6', 'GB%':'42.6', 'ERA':'2.57', 'FIP':'3.13','WAR':'5.4'},
-    {'num': '4', 'name': 'Cole Ragans', 'team': 'KCR', 'IP': '186.1', 'K%': '29.3', 'BB%': '8.8', 'GB%':'40.7', 'ERA':'3.14', 'FIP':'2.99','WAR':'4.9'},
-    {'num': '5', 'name': 'Dylan Cease', 'team': 'SDP', 'IP': '189.1', 'K%': '29.4', 'BB%': '8.5', 'GB%':'33.8', 'ERA':'3.47', 'FIP':'3.10','WAR':'4.8'}
+    {'num': '1', 'name': 'Chris Sale', 'team': 'ATL', 'IP': '177.2', 'K%': '32.1', 'BB%': '5.6', 'GB%':'44.8', 'ERA':'2.38', 'FIP':'2.09','fWAR':'6.4'},
+    {'num': '2', 'name': 'Tarik Skubal', 'team': 'DET', 'IP': '192.0', 'K%': '30.3', 'BB%': '4.6', 'GB%':'45.7', 'ERA':'2.39', 'FIP':'2.49','fWAR':'5.9'},
+    {'num': '3', 'name': 'Zack Wheeler', 'team': 'PHI', 'IP': '200.0', 'K%': '29.5', 'BB%': '6.6', 'GB%':'42.6', 'ERA':'2.57', 'FIP':'3.13','fWAR':'5.4'},
+    {'num': '4', 'name': 'Cole Ragans', 'team': 'KCR', 'IP': '186.1', 'K%': '29.3', 'BB%': '8.8', 'GB%':'40.7', 'ERA':'3.14', 'FIP':'2.99','fWAR':'4.9'},
+    {'num': '5', 'name': 'Dylan Cease', 'team': 'SDP', 'IP': '189.1', 'K%': '29.4', 'BB%': '8.5', 'GB%':'33.8', 'ERA':'3.47', 'FIP':'3.10','fWAR':'4.8'}
 ]
 
-let pitchingHeader = ['#', 'Name', 'Team', 'IP', 'K%', 'BB%', 'GB%', 'ERA', 'FIP', 'WAR' ]
+let pitchingHeader = ['#', 'Name', 'Team', 'IP', 'K%', 'BB%', 'GB%', 'ERA', 'FIP', 'fWAR' ]
 
 let thead = document.getElementById('stats-thead');
 let tbody = document.getElementById('stats-tbody');
