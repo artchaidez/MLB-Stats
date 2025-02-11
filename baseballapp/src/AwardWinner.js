358962944980b4b788949e34afeff5d60ac85fff
artchaidez <artchaidez1995@gmail.com>
2025-02-11 14:21:19 -0600

    Added rotating arrow icon
    Added rotating arrow icon; works on clicking open content. Added css for different screen sizes

--- baseballapp/src/AwardWinner.js
+++ baseballapp/src/AwardWinner.js
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import BatterTable from "./BatterTable";
 import PitcherTable from "./PitcherTable";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 const AwardWinner = ({ data }) => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);  
+
+  const rotate = open ? "rotate(-90deg)" : "rotate(0)"
   const hitter = data.hitter;
 
   return (
@@ -20,6 +24,7 @@ const AwardWinner = ({ data }) => {
         <span className="collapse-title">
           {`${data.firstName} ${data.lastName} - ${data.city} ${data.team}`}
         </span>
+        <FontAwesomeIcon icon={faArrowLeft} style={{ transform: rotate, transition: "all 0.2s linear" }} />
       </div>
       {open && (
         hitter ? <BatterTable data={data}/> : <PitcherTable data={data}/>
