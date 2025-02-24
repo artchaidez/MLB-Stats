a64f183a7045d1faacbd3f488beba60f10887a99
artchaidez <artchaidez1995@gmail.com>
2025-02-24 14:52:15 -0600

    Leaders component created
    Leaders component created; working tabs to show different tables. Table html are hard coded in and todo is creating a component for the tables

--- baseballapp/src/Footer.js
+++ baseballapp/src/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 const Footer = () => {
     return (
     <footer className="footer">
-        <Link to="https://www.fangraphs.com/" className="footer-link">Fangraphs</Link> |
+        <Link to="https://www.fangraphs.com/" className="footer-link">Fangraphs</Link>
         <Link to="https://baseballsavant.mlb.com/" className="footer-link">Baseball-Savant</Link>
     </footer>
     )
