42b48a686c53afe55deeef0eb024eb0082d8498d
artchaidez <artchaidez1995@gmail.com>
2025-05-16 11:41:44 -0500

    addHof updates totalVotes
    addHof method updates totalVotes value if player already exists in db (implying they have at least one vote)

--- baseball-backend/src/main/java/com/mlb_stats/baseball/hof/HofService.java
+++ baseball-backend/src/main/java/com/mlb_stats/baseball/hof/HofService.java
@@ -3,11 +3,24 @@ package com.mlb_stats.baseball.hof;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class HofService {
 
     @Autowired
     private HofRepository hofRepository;
 
-    public void addHof(HofModel hof) {hofRepository.save(hof);}
+    public void addHof(HofModel hof) {
+        Optional<HofModel> checkHof = hofRepository.findById(hof.getId());
+
+        if (checkHof.isPresent())
+        {
+            hof.setTotalVotes(checkHof.get().getTotalVotes() + 1);
+            hofRepository.save(hof);
+        } else {
+            hofRepository.save(hof);
+        }
+
+    }
 }
